548aae3781ca37bf7ca2401a949a25ea61eb319d
JesusGarSan <128600457+JesusGarSan@users.noreply.github.com>
2025-02-12 13:46:32 +0100

    Home text

--- home.py
+++ home.py
@@ -6,4 +6,7 @@ import config.init
 
 config.init.init("Home", "👁️")
 st.divider()
+st.markdown("""
+This web application is a fanmade recolection of the different episodes from the podcast "The Magnus Archives".
+""")
 
